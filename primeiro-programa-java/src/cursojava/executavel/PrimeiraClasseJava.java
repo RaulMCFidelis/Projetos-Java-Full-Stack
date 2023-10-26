b679eeabe0e802c4be8c34dc44ec8231fdac9a5e
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-10-25 23:21:59 -0300

    Estrutura de declaração de variáveis

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,22 +1,40 @@
 package cursojava.executavel;
 
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
 public class PrimeiraClasseJava {
 
 	/*Main é um metodo auto executavel em JAVA*/
 	public static void main(String[] args) {
 		
 		/*Escreve texto no console*/
-		System.out.println("Meu primeiro programa em JAVA! - Curso Formação Java Web");
-		System.out.println("Java é orientado a objetos!");
+		/*System.out.println("Meu primeiro programa em JAVA! - Curso Formação Java Web");
+		System.out.println("Java é orientado a objetos!");*/
 		
 		/*O valor padrão sempre será Zero*/
-		int numero;
-		
+		/*int numero;		
 		int numero2 = 0;
+		System.out.println(numero2);*/
+		/*boolean logica;
+		boolean logica2;
+		int numero;
+		int numero2;
+		short numero4;*/
 		
-		System.out.println(numero2);
+		int maiorIdade = 18;
 		
+		int idosoIdade = 60;
 		
+		int jovemIdade = 16;
 		
+		int criancaIdade = 7;
+		
+		String cpf = "227.830.678-20";
+		
+		System.out.println("Maior idade é = " + maiorIdade);
+		System.out.println("Idoso a idade é = " + idosoIdade);
+		System.out.println("Jovem a idade é = " + jovemIdade);
+		System.out.println("Criança a idade é = " + criancaIdade);
+	
 	}
 }
