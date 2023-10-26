b2652e6089208164fb46e306e03978d7188dcc22
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-10-25 23:38:04 -0300

    Fazendo operações entre números inteiros

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,48 +2,17 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*Variável Global é acessivel a todos e o seu valor é compartilhado*/
-	
-	static int maiorIdadeGlobal = 30;
 
-	/*Main é um metodo auto executavel em JAVA*/
+	/*Main é um método auto executavel em JAVA*/
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console*/
-		/*System.out.println("Meu primeiro programa em JAVA! - Curso Formação Java Web");
-		System.out.println("Java é orientado a objetos!");*/
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
 		
-		/*O valor padrão sempre será Zero*/
-		/*int numero;		
-		int numero2 = 0;
-		System.out.println(numero2);*/
-		/*boolean logica;
-		boolean logica2;
-		int numero;
-		int numero2;
-		short numero4;*/
-		/*
-		int maiorIdade = 18;
-	 	int idosoIdade = 60;
-		int jovemIdade = 16;
-		int criancaIdade = 7;
-		String cpf = "227.830.678-20";
-		System.out.println("Maior idade é = " + maiorIdade);
-		System.out.println("Idoso a idade é = " + idosoIdade);
-		System.out.println("Jovem a idade é = " + jovemIdade);
-		System.out.println("Criança a idade é = " + criancaIdade);
-	*/
-		
-		/*Variável Local, pois, pertence somente a este método e o valor fica dentro do metodo*/
-		int maiorIdade = 18;
-		System.out.println("Valor da variável loca = " + maiorIdade);
-		
-		metodo2();
-	}
+		System.out.println("Média das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
 	
-	public static void metodo2() {
-		int mediaAno = 50;
-		System.out.println("Valor da variável Global = " + maiorIdadeGlobal); 
 		
 	}
 }
