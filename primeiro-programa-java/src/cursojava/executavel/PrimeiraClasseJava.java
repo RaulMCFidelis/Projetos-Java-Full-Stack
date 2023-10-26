c5a19d61d3c83bccdc6b15ca97b2a0e8bea2af89
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-10-26 00:08:28 -0300

    Concatenação de variáveis

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,11 +6,17 @@ public class PrimeiraClasseJava {
 	/*Main é um método auto executavel em JAVA*/
 	public static void main(String[] args) {
 		
-		/*String no Java sempre será um texto de tamanho qualquer*/
-		String nome;
-		String cpf;
-		String telefone;
-		String endereco;
+		/*Concatenação = Unir ou juntar dados*/
+		String nome = "Raul Maximiliano";
+		String cpf = "364.231.418-00";
+		String telefone = "(11) 910105262";
+		String endereco = "São Paulo - SP";
+		int idade = 28;
+		
+		String saida = "Meu nome é " + nome + "com o CPF: " + cpf + "meu contato é: " + telefone + "e moro em: " + endereco
+				+ " e tenho: " + idade + " anos";
+		
+		System.out.println(saida);
 		
 	}
 }
